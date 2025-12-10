e027518adc156a1689e25c7ee5e6058339f6366f
ZhanpengW <127912112+ZhanpengW@users.noreply.github.com>
2025-12-10 13:57:15 +0800

    Add files via upload

---
+++ LATENT_WILD2DOG.sh
@@ -0,0 +1,199 @@
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 10 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 20 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 30 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 40 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 50 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 60 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 70 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 80 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 90 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 100 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 110 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 120 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 130 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 140 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 150 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 160 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 170 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 180 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 190 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 200 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 210 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 220 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 230 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 240 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 250 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 260 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 270 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 280 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 290 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 300 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 310 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 320 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 330 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 340 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 350 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 360 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 370 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 380 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 390 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 400 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 410 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 420 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 430 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 440 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 450 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 460 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 470 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 480 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 490 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 500 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 510 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 520 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 530 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 540 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 550 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 560 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 570 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 580 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 590 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 600 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 610 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 620 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 630 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 640 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 650 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 660 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 670 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 680 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 690 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 700 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 710 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 720 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 730 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 740 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 750 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 760 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 770 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 780 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 790 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 800 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 810 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 820 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 830 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 840 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 850 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 860 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 870 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 880 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 890 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 900 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 910 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 920 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 930 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 940 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 950 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 960 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 970 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 980 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 990 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/wild2dog_latent.py --diffusion_steps 1000 --truncated_step 1000 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
---
+++ TRANSLATION_MALE2FEMALE.sh
@@ -0,0 +1,19 @@
+python scripts/male2female_translation.py --diffusion_steps 4000 --truncated_step 2900 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/male2female_translation.py --diffusion_steps 4000 --truncated_step 2800 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/male2female_translation.py --diffusion_steps 4000 --truncated_step 2700 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/male2female_translation.py --diffusion_steps 4000 --truncated_step 2600 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/male2female_translation.py --diffusion_steps 4000 --truncated_step 2500 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/male2female_translation.py --diffusion_steps 4000 --truncated_step 2400 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/male2female_translation.py --diffusion_steps 4000 --truncated_step 2300 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/male2female_translation.py --diffusion_steps 4000 --truncated_step 2200 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/male2female_translation.py --diffusion_steps 4000 --truncated_step 2100 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
+
+python scripts/male2female_translation.py --diffusion_steps 4000 --truncated_step 2000 --image_size 512 --noise_schedule linear --num_channels 128 --num_res_blocks 3
